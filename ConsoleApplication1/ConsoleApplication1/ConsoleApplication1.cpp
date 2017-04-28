0482c0334f6c5420dfe0a3d1fbd0787257219a86
evgen.fesuk <evgen.fesuk@gmail.com>
2017-04-28 18:57:26 +0300

    new iClass

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "iClass.h"
+
+
+int main()
+{
+	iClass c;
+	//iClass q = new iClass;
+	system("pause");
+    return 0;
+}
+
